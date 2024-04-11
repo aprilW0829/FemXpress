485b9f7f6f533b767c734e79599f98554528e5e7
wangxin970829 <93782250+wangxin970829@users.noreply.github.com>
2024-04-11 21:20:41 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from pathlib import Path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FemXpress",
+    version="v1.0.0",
+    author="Xin Wang",
+    author_email="wangxin970829@163.com",
+    description="Tool that classifies the source of X chromosome inactivation of each female cell",
+    long_description=long_description,
+    #long_description_content_type="",
+    #url="",
+    install_requires=[
+        l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL v3 or later",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
+
