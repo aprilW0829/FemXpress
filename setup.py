9932bed71c1f139fe4652b5f40ea9034cbd66097
wangxin970829 <93782250+wangxin970829@users.noreply.github.com>
2024-04-20 11:03:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,17 @@ setuptools.setup(
     author_email="wangxin970829@163.com",
     description="Tool that classifies the source of X chromosome inactivation of each female cell",
     long_description=long_description,
+    url='https://github.com/wangxin970829/FemXpress',
+    include_package_data=True,
+    package_data={
+    'FemXpress':['scripts/*']
+    },
     #long_description_content_type="",
     #url="",
     install_requires=[
         l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
     ],
+    packages=setuptools find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL v3 or later",
