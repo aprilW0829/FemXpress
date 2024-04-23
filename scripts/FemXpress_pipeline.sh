7dd8319dea83dc645973a73dde00207480b5c3bb
wangxin970829 <93782250+wangxin970829@users.noreply.github.com>
2024-04-23 09:56:25 +0800

    Update FemXpress_pipeline.sh

--- scripts/FemXpress_pipeline.sh
+++ scripts/FemXpress_pipeline.sh
@@ -2,8 +2,8 @@
 
 # step1:
 # data preprocess
-python FemXpress_1.py -b possorted_genome_bam.bam -g genome.fa -e meta.txt -r rmsk.txt
+python FemXpress_1.py -b possorted_genome_bam.bam -g genome.fa -e meta.txt -r rmsk.txt > nohup_real1.txt
 
 # step2:
 # FemXpress inference clustering and escape genes
-python FemXpress_2.py -m result_matrix4.csv -a /data2/wangxin/database/genome/gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf > nohup_real.txt
+python FemXpress_2.py -m result_matrix4.csv -a /data2/wangxin/database/genome/gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf > nohup_real2.txt
