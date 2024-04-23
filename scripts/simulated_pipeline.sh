b3993d174f8888e2c964c3d015457c4911c2432c
wangxin970829 <93782250+wangxin970829@users.noreply.github.com>
2024-04-23 20:22:59 +0800

    Update simulated_pipeline.sh

--- scripts/simulated_pipeline.sh
+++ scripts/simulated_pipeline.sh
@@ -1,7 +1,7 @@
-### 1：下载源数据GSE203275，运行cellranger 
+### 1：Download the source data GSE203275 and run CellRanger. 
 
-### 2: 把比对结果加上MD标签 
-# nohup samtools calmd -@ 36 -bAr possorted_genome_bam.bam /path_to_fa/genome.fa > possorted_baq_ne_genome_bam.bam &
+### 2: Add the MD labels to align results.
+nohup samtools calmd -@ 36 -bAr possorted_genome_bam.bam /path_to_fa/genome.fa > possorted_baq_ne_genome_bam.bam &
 
-### 3：把reads修改为reference,再修改为1000 genome trio中的HG000405的能分辨出父母来源的SNP
+### 3：Change the reads to reference, and then change to the SNP of the HG000405 in the 1000 genome trio to distinguish the origin of the parent.
 nohup python change_to_ref_to_405_v2,py &
