ec2f6decf09148bef63f19945399f6738cc6ee92
aprilW <93782250+aprilW0829@users.noreply.github.com>
2024-10-19 15:58:57 +0800

    Update FemXpress_pipeline.sh

--- scripts/FemXpress_pipeline.sh
+++ scripts/FemXpress_pipeline.sh
@@ -2,8 +2,8 @@
 
 # step1:
 # data preprocess
-python FemXpress_1.py -b possorted_genome_bam.bam -g genome.fa -e meta.txt -r rmsk.txt > nohup_real1.txt
+python FemXpress_1.py -b possorted_genome_bam.bam -g genome.fa -e meta.txt -r rmsk.txt -n sample > nohup_real1.txt
 
 # step2:
 # FemXpress inference clustering and escape genes
-python FemXpress_2.py -m result_matrix4.csv -a gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf > nohup_real2.txt
+python FemXpress_2.py -m result_matrix4.csv -a gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf -n sample > nohup_real2.txt
