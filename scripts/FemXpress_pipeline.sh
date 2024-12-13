824366ecb2ef8d4ef466f9505f30f5d288c09049
aprilW <93782250+aprilW0829@users.noreply.github.com>
2024-12-13 17:52:20 +0800

    Add files via upload

--- scripts/FemXpress_pipeline.sh
+++ scripts/FemXpress_pipeline.sh
@@ -1,9 +1,8 @@
-#!usr/bin/bash
-
-# step1:
-# data preprocess
-python FemXpress_1.py -b possorted_genome_bam.bam -g genome.fa -e meta.txt -r rmsk.txt -n sample -c 2 > nohup_real1.txt
-
-# step2:
-# FemXpress inference clustering and escape genes
-python FemXpress_2.py -m result_matrix4.csv -a gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf -n sample > nohup_real2.txt
+python FemXpress.py \
+	-b '/data5/wangxin/20220923fgh_mouse_brain/mouse_brain_cellranger/220731C_mousebrain/outs1/possorted_genome_bam.bam' \
+	-g '/data2/wangxin/database/genome/mm10.fa' \
+	-e '/data5/wangxin/20220923fgh_mouse_brain/mouse_brain_cellranger/meta/mb_brain_meta.txt' \
+	-r '/data2/wangxin/database/processed_genome/mm10.rmsk.txt' \
+	-n 'mouse' \
+	-a '/data2/wangxin/database/genome/gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf' \
+	-c 2
