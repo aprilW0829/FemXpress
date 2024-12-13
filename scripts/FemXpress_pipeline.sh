bca946b279b33a2e5f9d456be8f093d1f1c1e8b4
aprilW <93782250+aprilW0829@users.noreply.github.com>
2024-12-13 17:53:07 +0800

    Update FemXpress_pipeline.sh

--- scripts/FemXpress_pipeline.sh
+++ scripts/FemXpress_pipeline.sh
@@ -1,8 +1,8 @@
 python FemXpress.py \
-	-b '/data5/wangxin/20220923fgh_mouse_brain/mouse_brain_cellranger/220731C_mousebrain/outs1/possorted_genome_bam.bam' \
-	-g '/data2/wangxin/database/genome/mm10.fa' \
-	-e '/data5/wangxin/20220923fgh_mouse_brain/mouse_brain_cellranger/meta/mb_brain_meta.txt' \
-	-r '/data2/wangxin/database/processed_genome/mm10.rmsk.txt' \
+	-b 'possorted_genome_bam.bam' \
+	-g 'mm10.fa' \
+	-e 'mb_brain_meta.txt' \
+	-r 'mm10.rmsk.txt' \
 	-n 'mouse' \
-	-a '/data2/wangxin/database/genome/gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf' \
+	-a 'gencode.vM25.chr_patch_hapl_scaff.basic.annotation.gtf' \
 	-c 2
